2630ce0792d2077e575c41380f6bb42eda36d4e9
Anton Bezmelnitsin <a.bezmelnitsin.a@yandex.ru>
2024-05-26 23:02:27 +0500

    Initial commit

---
+++ src/main/java/com/example/java_lab_10/JavaLab10Application.java
@@ -0,0 +1,13 @@
+package com.example.java_lab_10;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaLab10Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaLab10Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/java_lab_10/JavaLab10ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.java_lab_10;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaLab10ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
